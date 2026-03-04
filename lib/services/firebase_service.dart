4de2c0015ab37df66047d9d45c92c0f394b15bd1
keval <keval.thakar@discus.solutions>
2026-03-05 03:46:40 +0530

    This implementation is intended to showcase Firebase connectivity and backup capability.

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import '../models/todo_model.dart';
 
 class FirebaseService {
